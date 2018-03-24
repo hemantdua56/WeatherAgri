1dd9e2ebd6f35ebd59d195f17579605f8b2497eb
hemant <hemant.dua56@gmail.com>
2018-03-24 23:16:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hemant/weatheragri/ParseResult.java
@@ -0,0 +1,3 @@
+package com.example.hemant.weatheragri;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
---
+++ app/src/main/java/com/example/hemant/weatheragri/TaskResult.java
@@ -0,0 +1,3 @@
+package com.example.hemant.weatheragri;
+
+public enum TaskResult { SUCCESS, BAD_RESPONSE, IO_EXCEPTION, TOO_MANY_REQUESTS; }
